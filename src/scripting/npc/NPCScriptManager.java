18a73e2142ed62e5cbdbdb90d126ab752e43f918
Hendi <32822533+Hendi48@users.noreply.github.com>
2021-11-20 17:13:38 +0100

    So who had this stupid idea to duplicate NPC scripts per world anyway?

--- src/scripting/npc/NPCScriptManager.java
+++ src/scripting/npc/NPCScriptManager.java
@@ -55,10 +55,10 @@ public class NPCScriptManager extends AbstractScriptManager {
             cms.put(c, cm);
             Invocable iv = null;
             if (filename != null) {
-                iv = getInvocable("npc/world" + c.getWorld() + "/" + filename + ".js", c);
+                iv = getInvocable("npc/" + filename + ".js", c);
             }
             if (iv == null) {
-                iv = getInvocable("npc/world" + c.getWorld() + "/" + npc + ".js", c);
+                iv = getInvocable("npc/" + npc + ".js", c);
             }
             if (iv == null || NPCScriptManager.getInstance() == null) {
                 dispose(c);
@@ -105,7 +105,7 @@ public class NPCScriptManager extends AbstractScriptManager {
         MapleClient c = cm.getClient();
         cms.remove(c);
         scripts.remove(c);
-        resetContext("npc/world" + c.getWorld() + "/" + cm.getNpc() + ".js", c);
+        resetContext("npc/" + cm.getNpc() + ".js", c);
     }
 
     public void dispose(MapleClient c) {
